a2a7fcd5c222c15685ed26e93a6c89318f9f5d92
saulpadilla10 <saulp1021@yahoo.com>
2023-06-03 00:13:20 -0700

    Car

---
+++ Classes/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+	
+			public string? Make { get; set; }
+			public string? Model { get; set; }
+			public int Year { get; set; }
+		
+
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var vehicle = new Car()
+            {
+                Make = "Dodge",
+                Model = "Dart",
+                Year = 1985
+            };
+
+            var carList = new List<Car>() { vehicle };
+
+            Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+
+
         }
     }
 }
